f9c88ec1595b3ef0d422fc027cbae5ccd51f1e04
Knockturne42 <dcl.thibauds@18pixel.fr>
2018-10-24 23:45:54 +0200

    header pokemon

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,26 @@
 var playerOne;
 var playerTwo;
+var player = document.getElementById('player');
+var chooseBlock = document.getElementById('choose');
+var playButton = document.getElementById('play');
 
-function chooseDisc(playerOne, playerTwo) {
+function playGame() {
+	playButton.addEventListener("click", function(){
+		if (!playerOne) {
+			chooseBlock.style.display = "block";
+			setDisc(playerOne, playerTwo);
+		}
+	});
+}
+
+/*function displaySetDisc(playerOne, playerTwo) {
+	if (!playerOne) {
+		chooseBlock.style.display = "block";
+		setDisc(playerOne, playerTwo);
+	}
+}*/
+
+function setDisc(playerOne, playerTwo) {
 	var choice = document.getElementsByClassName('discChoose');
 	for (var i = 0; i < choice.length; i++) {
 		(function(index, playerOne, playerTwo){
@@ -16,6 +35,8 @@ function logoPlayer(choice) {
 	if (!playerOne)
 	{
 		playerOne = choice.src;
+		player.innerHTML = "Joueur 2";
+		player.style.backgroundColor = "rgba(50, 250, 200, 1)"
 		console.log(playerOne);
 		choice.style.backgroundColor = "rgba(200, 250, 50, 1)";
 	}
@@ -24,9 +45,14 @@ function logoPlayer(choice) {
 		playerTwo = choice.src;
 		console.log(playerTwo);
 		choice.style.backgroundColor = "rgba(50, 250, 200, 1)";
+		setTimeout(function(){ chooseBlock.style.display = "none"; }, 3000);
+		player.style.backgroundColor = "grey";
+		player.innerHTML = "3";
+		setTimeout(function(){ player.innerHTML = "2"; }, 1000);
+		setTimeout(function(){ player.innerHTML = "1"; }, 2000);
 	}
 	else {
 	}
 }
 
-chooseDisc()
+playGame();
